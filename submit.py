ae8fc87ddafebc7612854708142726008eca579a
sevenquarkoniums <sevenquarkoniums@gmail.com>
2020-01-28 12:52:45 -0800

    allocation exp

--- submit.py
+++ submit.py
@@ -4,7 +4,8 @@ from subprocess import call
 def main():
     s = submit()
     #s.miniMD()
-    s.withOSU(timelimit='00:30:00', isSingle=1, isMail=0)
+    #s.withOSU(timelimit='00:30:00', isSingle=1, isMail=1)
+    s.allocation(timelimit='00:30:00', isSingle=1, isMail=1)
 
 class submit:
     def miniMD(self):
@@ -15,8 +16,16 @@ class submit:
     def withOSU(self, timelimit, isSingle, isMail):
         singleton = '-d singleton ' if isSingle else ''
         mail = '--mail-type=END ' if isMail else ''
-        for run in range(0, 20):
-            command = 'sbatch -N 64 --account=m3410 -q regular -C haswell %s-t %s -J withOSU %s-o $HOME/allocation/OSUresults/Cong24c_%d.out withOSU.py' % (mail, timelimit, singleton, run)
+        for run in range(0, 5):
+            #command = 'sbatch -N 64 --account=m888 -q premium -C haswell %s-t %s -J withOSU %s-o $HOME/allocation/OSUresults/Cong_c32_ins8_%d.out withOSU.py 8' % (mail, timelimit, singleton, run)
+            command = 'sbatch -N 64 --account=m888 -q premium -C haswell %s-t %s -J withOSU %s-o $HOME/allocation/OSUresults/Cong_c32_ins%d_%d.out withOSU.py %d' % (mail, timelimit, singleton, 5+run, 0, 5+run)
+            call(command, shell=True)
+
+    def allocation(self, timelimit, isSingle, isMail):
+        singleton = '-d singleton ' if isSingle else ''
+        mail = '--mail-type=END ' if isMail else ''
+        for run in range(0, 1):
+            command = 'sbatch -N 96 --account=m888 -q premium -C haswell %s-t %s -J withOSU %s-o $HOME/allocation/OSUresults/alloc_c32_ins10_%d.out withOSU.py 10' % (mail, timelimit, singleton, run)
             call(command, shell=True)
 
 if __name__ == '__main__':
--- withOSU.py
+++ withOSU.py
@@ -3,12 +3,14 @@ import subprocess
 import os
 import signal
 import time
+import sys
 
 def main():
-    time.sleep(5)
+    time.sleep(10)
     w = withOSU()
     #w.testOSU()
-    w.congestion(withCongestor=1, core=24)
+    #w.congestion(withCongestor=1, core=32, instance=int(sys.argv[1]))
+    w.allocation(instance=int(sys.argv[1])
 
 class withOSU:
     def __init__(self):
@@ -47,58 +49,67 @@ class withOSU:
         nodestr += ']'
         return nodestr
         
-    def testOSU(self):
-        N = 32
-        ntasks = 32*N
-        msize = 4096
-        osulist = self.abbrev(self.jumpOne(self.nodelist, N))
-        exe = '/global/homes/z/zhangyj/osu/osu-micro-benchmarks-5.6.2/install/libexec/osu-micro-benchmarks/mpi/collective/osu_alltoall'
-        command = 'srun -N %d --ntasks %d --nodelist=%s -C haswell %s -m %d:%d -i 100\n' % (N, ntasks, osulist, exe, msize, msize)
-        print(command)
-        print(subprocess.check_output(['date']).decode('utf-8'))
-        output = subprocess.check_output(command.split(' ')).decode('utf-8')
-        #run = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-        #output = run.communicate()[0].strip()
-        print(output)
-        print(subprocess.check_output(['date']).decode('utf-8'))
-        #osu = subprocess.Popen(command, preexec_fn=os.setsid, shell=True)
-
-    def congestion(self, withCongestor, core=32):
-        N = 32
+    def startOSU(self, N, core, instance):
         ntasks = core*N
         msize = 4096
-        osulist = self.abbrev(self.jumpOne(self.nodelist, N))
+        self.usedNodes = self.jumpOne(self.nodelist, N)
+        osulist = self.abbrev(self.usedNodes)
         exe = '/global/homes/z/zhangyj/osu/osu-micro-benchmarks-5.6.2/install/libexec/osu-micro-benchmarks/mpi/collective/osu_alltoall'
         taskspernode = '--ntasks-per-node=%d ' % core if core != 32 else ''
         command = 'srun -N %d --ntasks %d --nodelist=%s %s-C haswell %s -m %d:%d -i 90000' % (N, ntasks, osulist, taskspernode, exe, msize, msize)
-            # iter 1000 for 32 nodes is approx 20 min.
-        if withCongestor:
+        procs = []
+        for i in range(instance):
             osu = subprocess.Popen(command, shell=True, preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print('osu started.')
-            #stdout, stderr = osu.communicate()
-            #print(stdout)
-            #print(stderr)
-        else:
-            print('osu not started.')
+            print('osu %d started.' % i)
+            procs.append(osu)
+        return procs
 
-        N = 32
+    def startApp(self, N, alloc):
+        print('Allocation method: %s' % alloc)
         ntasks = 32*N
-        appnodeList = [x for x in self.nodelist if x not in self.jumpOne(self.nodelist, N)]
+        unUsedNodes = [x for x in self.nodelist if x not in self.usedNodes]
+        if alloc == 'bad':
+            appnodeList = unUsedNodes[:N]
+        elif alloc == 'good':
+            appnodeList = unUsedNodes[N:]
         liststr = ','.join(['nid{0:05d}'.format(y) for y in appnodeList])
         print('app-node list:')
         print(liststr)
         appcmd = 'module load openmpi; cd $HOME/miniMD/ref; '
         appcmd += 'time mpirun -np %d --host %s $HOME/miniMD/ref/miniMD_openmpi -n 160000; ' % (ntasks, liststr) # 160000.
-        print('startTime: ' + subprocess.check_output(['date']).decode('utf-8'))
+        print('startTime:%s:' % (alloc) + subprocess.check_output(['date']).decode('utf-8'))
         apprun = subprocess.Popen(appcmd, stdout=subprocess.PIPE, shell=True)
         output = apprun.communicate()[0].strip()
         print(output.decode('utf-8'))
-        print('endTime: ' + subprocess.check_output(['date']).decode('utf-8'))
+        print('endTime:%s:' % (alloc) + subprocess.check_output(['date']).decode('utf-8'))
+        print()
+
+    def congestion(self, withCongestor, core, instance):
+        if withCongestor:
+            procs = self.startOSU(N=32, core=core, instance=instance)
+        else:
+            print('osu not started due to setting.')
+
+        self.startApp(N=32, alloc='bad')
 
         if withCongestor:
             print('is osu running:')
+            for i in range(instance):
+                osu = procs.pop()
+                print(osu.poll() == None)
+                os.killpg(os.getpgid(osu.pid), signal.SIGTERM)
+
+    def allocation(self, instance, core=32):
+        procs = self.startOSU(N=32, core=core, instance=instance)
+        self.startApp(N=32, alloc='good')
+        self.startApp(N=32, alloc='bad')
+
+        print('is osu running:')
+        for i in range(instance):
+            osu = procs.pop()
             print(osu.poll() == None)
             os.killpg(os.getpgid(osu.pid), signal.SIGTERM)
+
 
 if __name__ == '__main__':
     main()
